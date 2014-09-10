57b1168698853f0186756a9d65c792a9d7ad3531
Michael Barreiro <Mikeb_barreiro@yahoo.com>
2014-09-10 00:54:13 -0500

    Pre-Load Page with googleAPI and img

--- app/static/js/user/home.js
+++ app/static/js/user/home.js
@@ -1,8 +1,17 @@
+/* global google:true */
+
 (function(){
   'use strict';
+  var map;
 
   $(document).ready(function(){
+    initMap(36, -86, 2);
   });
 
-})();
+  function initMap(lat, lng, zoom){
+    var styles = [{'stylers':[{'hue':'#ff1a00'},{'invert_lightness':true},{'saturation':-100},{'lightness':33},{'gamma':0.5}]},{'featureType':'water','elementType':'geometry','stylers':[{'color':'#2D333C'}]}],
+        mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP, styles: styles};
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
 
+})();
