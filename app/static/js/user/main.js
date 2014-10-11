50e2985d2196e08f52a1b2d9b6bb13133c41a22d
Michael Barreiro <Mikeb_barreiro@yahoo.com>
2014-10-11 00:29:49 -0500

    New image background

--- app/static/js/user/main.js
+++ app/static/js/user/main.js
@@ -2,7 +2,7 @@
   'use strict';
 
   $(document).ready(function(){
-    //$('#toggle_showshooshi').click(onClick);
+    $('#toggle_showshooshi').click(onClick);
   });
 ////////////////// WAYPOINTS START ///////////////////
 
@@ -32,11 +32,12 @@
 
 ////////////////// WAYPOINTS END ///////////////////
 
-  //function onClick(){
+  function onClick(){
     //debugger;
+    document.getElementById('bleeP').play();
     //var bleep = new Audio();
     //bleep.src = '/audio/bleep.mp3';
-  //}
+  }
 
 })();
 
